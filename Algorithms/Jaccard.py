eda11bd19a651d96fa702d9cea4b00a994b3dd64
andrewma <35508014+andrewma@users.noreply.github.com>
2020-04-22 22:47:32 -0400

    changed algorithms spelling

--- Algorithms/Jaccard.py
+++ Algorithms/Jaccard.py
@@ -9,10 +9,10 @@
 ##Very rudimentary but can be used to start with
 
 #For example, if there are 4 categories of a man class. Age, height, weight and race a
-#and we want ot find out the similarity. We can assign age as .2, weight as .4, hight as .1 and 
+#and we want ot find out the similarity. We can assign age as .2, weight as .4, hight as .1 and
 #race as .3
 
-#Lets say the boys are boh white, both weight 200 pounds, one is 12 and the other is 16 yo and one is 
+#Lets say the boys are boh white, both weight 200 pounds, one is 12 and the other is 16 yo and one is
 # 6 feet and the other is 5 feet
 
 #In this case the similarity will be .7
@@ -20,10 +20,8 @@
 #This is a super simple example and this should be expanded upon but is a start
 #
 #
-#  
 #
 #
 #
 #
-
-
+#
